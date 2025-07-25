ca7282fd27bbcb53ec51ac9c7067fa73eb0f4e66
Asendar1 <hamzahtaimour@gmail.com>
2025-07-25 20:16:54 +0300

    fix whatis.py to not be run while imported

--- module00/ex04/whatis.py
+++ module00/ex04/whatis.py
@@ -6,11 +6,12 @@ def check_even_odd(value):
 	else:
 		print("Value is odd")
 
-if len(sys.argv) > 2:
-	print("AssertionError: More than one argument given")
-elif len(sys.argv) == 2:
-	try:
-		value = int(sys.argv[1])
-		check_even_odd(value)
-	except ValueError:
-		print("AssertionError: Argument is not an integer")
+if __name__ == "__main__":
+	if len(sys.argv) > 2:
+		print("AssertionError: More than one argument given")
+	elif len(sys.argv) == 2:
+		try:
+			value = int(sys.argv[1])
+			check_even_odd(value)
+		except ValueError:
+			print("AssertionError: Argument is not an integer")
