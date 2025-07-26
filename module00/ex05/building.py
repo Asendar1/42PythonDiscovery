3d03395b3ca332c47f413b4b33c4ae7b0cd7e081
Asendar1 <hamzahtaimour@gmail.com>
2025-07-26 14:31:50 +0300

    ex05 done

--- module00/ex05/building.py
+++ module00/ex05/building.py
@@ -1,1 +1,39 @@
+import sys
+
+def string_info(string):
+	__doc__ = "Gives info about the string provided"
+
+	upper = 0
+	lower = 0
+	digit = 0
+	space = 0
+	pun = 0
+
+	for c in string:
+		if (c.isupper()):
+			upper += 1
+		elif (c.islower()):
+			lower += 1
+		elif (c.isdigit()):
+			digit += 1
+		elif (c.isspace()):
+			space += 1
+		else:
+			pun += 1
+
+	print(f"The text contains {len(string)} characters")
+	print(f"Uppercase: {upper}")
+	print(f"Lowercase: {lower}")
+	print(f"Digits: {digit}")
+	print(f"Spaces: {space}")
+	print(f"Punctuation: {pun}")
+
 if __name__ == "__main__":
+	if len(sys.argv) > 2:
+		print("AssertionError: More than one argument given")
+	elif len(sys.argv) == 2:
+		string_info(sys.argv[1])
+	elif len(sys.argv) == 1:
+		args = input("Please provide a string: ")
+		string_info(args)
+
